12cca688d4c4e5ac9abb9b88633ad781d6bb42c7
evansochieng <evansochieng121@gmail.com>
2023-09-13 21:51:03 +0300

    write code to find anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -5,5 +5,22 @@ class Anagram:
     def __init__(self, word):
         self.word = word
         
-    def match(self):
-        pass
+    def match(self, word_list):
+        # define an empty list to store the anagrams
+        anagrams = []
+        
+        # get the letters in the original word
+        orig_word_letters = [letter for letter in self.word]
+        
+        # loop over the words in the list
+        for my_word in word_list:
+            # get a list of the letters in the word
+            letters = [letter for letter in my_word]
+            
+            # check if the sorted version of the letters matches the initial word
+            if sorted(letters) == sorted(orig_word_letters):
+                anagrams.append(my_word)
+                
+        # return the anagrams
+        return anagrams
+        
